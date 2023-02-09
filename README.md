4e959f4442f9ce80d1bbdad01cb9e255ebd72a1b
Andrew Novac <16753077+novatorem@users.noreply.github.com>
2023-02-08 22:38:16 -0500

    Formatting + justify left

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/fa8b9790-39ed-4665-bb44-8589eaf09e91/deploy-status)](https://app.netlify.com/sites/mailnovac/deploys)
 
-## Use cases:
- - Hide email, phone numbers from scrapers
- - Hide download links
- - Special messages for humans
+## Use cases
 
+- Hide email, phone numbers from scrapers
+- Hide download links
+- Special messages for humans
 
-## To integrate this yourself:
- - Add the reCAPTCHA elements (`index.html` and `main.js`) to your page
- - Register for your reCAPTCHA [here](https://www.google.com/recaptcha/admin/create), using your static site host name
- - Obtain your `site key`, set it as `data-site-key` attribute in the reCAPTCHA form
- - Obtain your `secret key`, set it as a Netlify environment variable `CAPTCHA_SECRET=...`
- - Update your Netlify Function url (ending should resemble `.com/.netlify/functions/{function_name}`)
- - Optional: set a Netlify environment variable `SECRET_MESSAGE=..` to return a message, or bake it into your serverless function
- 
- 
+## To integrate this yourself
+
+- Add the reCAPTCHA elements (`index.html` and `main.js`) to your page
+- Register for your reCAPTCHA [here](https://www.google.com/recaptcha/admin/create), using your static site host name
+- Obtain your `site key`, set it as `data-site-key` attribute in the reCAPTCHA form
+- Obtain your `secret key`, set it as a Netlify environment variable `CAPTCHA_SECRET=...`
+- Update your Netlify Function url (ending should resemble `.com/.netlify/functions/{function_name}`)
+- Optional: set a Netlify environment variable `SECRET_MESSAGE=..` to return a message, or bake it into your serverless function
+
 For verification flow: https://developers.google.com/recaptcha/docs/verify  
 More Netlify Function examples: https://github.com/netlify/functions
--- src/main.js
+++ src/main.js
@@ -4,50 +4,50 @@ const base = new URL('https://mail.novac.dev')
 const url = new URL(captcha, base)
 
 const onsubmit = (e) => {
-    // prevent refresh
-    e.preventDefault()
-    // get client response token, used to match client on back end
-    const rt = grecaptcha.getResponse()
+	// prevent refresh
+	e.preventDefault()
+	// get client response token, used to match client on back end
+	const rt = grecaptcha.getResponse()
 
-    return fetch(`${url.toString()}?rt=${rt}`)
-        .then((res) => {
-            if (res.status === 200) {
-                return res.text().then((text) => {
-                    if (text === '') {
-                        text = '(No secret message set.)'
-                    }
-                    return text
-                })
-            } 
-            return 'You failed the captcha, please try again!'
-        }).then((text) => {
-            document.getElementById('secret-message').textContent = text
-            document.getElementById('secret-message').style.background = 'None'
-            if (text === 'You failed the captcha, please try again!') {
-                document.getElementById('mail-to').href = "https://support.google.com/recaptcha"
-            }
-            else {
-                document.getElementById('mail-to').href = "mailto:" + text
-                document.getElementById('copy-to-clipboard').style.display = "block"
-                
-                document.getElementById('form').style.display = "none"
-                document.getElementById('verificationText').style.display = "none"
-            }
-        }).catch((err) => {
-            console.log(err)
-        })
+	return fetch(`${url.toString()}?rt=${rt}`)
+		.then((res) => {
+			if (res.status === 200) {
+				return res.text().then((text) => {
+					if (text === '') {
+						text = '(No secret message set.)'
+					}
+					return text
+				})
+			}
+			return 'You failed the captcha, please try again!'
+		}).then((text) => {
+			document.getElementById('secret-message').textContent = text
+			document.getElementById('secret-message').style.background = 'None'
+			if (text === 'You failed the captcha, please try again!') {
+				document.getElementById('mail-to').href = "https://support.google.com/recaptcha"
+			}
+			else {
+				document.getElementById('mail-to').href = "mailto:" + text
+				document.getElementById('copy-to-clipboard').style.display = "block"
+
+				document.getElementById('form').style.display = "none"
+				document.getElementById('verificationText').style.display = "none"
+			}
+		}).catch((err) => {
+			console.log(err)
+		})
 }
 
 const f = document.getElementById('form')
 f.addEventListener('submit', onsubmit)
 
 function copyToClipboard() {
-    var copyText = document.getElementById('secret-message');
-    navigator.clipboard.writeText(copyText.textContent);
+	var copyText = document.getElementById('secret-message');
+	navigator.clipboard.writeText(copyText.textContent);
 
-    const div = document.createElement('div')
-    div.className = 'toast'
-    div.innerHTML = "Copied to clipboard!"  
-    setTimeout(function() { div.remove() }, 5000);
-    document.body.appendChild(div);
+	const div = document.createElement('div')
+	div.className = 'toast'
+	div.innerHTML = "Copied to clipboard!"
+	setTimeout(function () { div.remove() }, 5000);
+	document.body.appendChild(div);
 }
